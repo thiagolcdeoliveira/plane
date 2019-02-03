617d096582a16704fd0e1942b7ea847e80b7d914
thiago <thiagolocatellicdeoliveira@gmail.com>
2019-02-03 03:20:25 -0200

    HTML: cliente produto pedido criados

--- plane/settings.py
+++ plane/settings.py
@@ -14,6 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR, ]
+STATIC_URL = '/static/'
 
 
 # Quick-start development settings - unsuitable for production
--- planeapp/urls.py
+++ planeapp/urls.py
@@ -4,5 +4,5 @@ from django.conf.urls import url, include
 from planeapp.views.produto import ProdutoListView
 
 urlpatterns=[
-    url(r'^/$', ProdutoListView.as_view(), name='produto-list'),
+    url(r'^$', ProdutoListView.as_view(), name='produto-list'),
 ]
